b330c9040f3c8bbd3905b124983c80ce171eb4f9
guifurst <guisouza2008@hotmail.com>
2015-11-23 19:14:16 -0200

    Update Exerc3.java

--- Exerc3.java
+++ Exerc3.java
@@ -1,11 +1,7 @@
 class Aluno{
-    string nome;
     int periodo;
     double coef;
 public:
-    Aluno(string novo_aluno);
-    Aluno(string novo_aluno, int novo_periodo, int novo_coef);
-    ~Aluno();
     void inserePeriodo(int novo_periodo);
     void insereCoef(double novo_coef);
     void ImprimeDados();
